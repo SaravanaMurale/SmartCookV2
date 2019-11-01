371a564441eb754de5a1a305c0c110e5ae16c059
murali <saravanamurali24@gmail.com>
2019-11-01 14:42:36 +0530

    Sqlite Manager

---
+++ app/src/main/java/com/clj/blesample/databasemanager/SqliteManager.java
@@ -0,0 +1,31 @@
+package com.clj.blesample.databasemanager;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class SqliteManager extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "farmersgen";
+    public static final int DATABASE_VERSION = 1;
+
+
+
+    public SqliteManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+}
