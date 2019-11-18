8492be0040e12f162ddbc792cceb8e35a5b24140
murali <saravanamurali24@gmail.com>
2019-11-18 13:02:31 +0530

    whistle and vessel UI changes commit

--- app/src/main/java/com/clj/blesample/DummyFragment.java
+++ app/src/main/java/com/clj/blesample/DummyFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sdsmdg.harjot.crollerTest.Croller;
@@ -18,6 +19,10 @@ public class DummyFragment extends Fragment {
 
     TextView textView,leftBurner,rightBurner;
     Croller crollerTop,crollerLeft,crollerRight;
+
+    ImageView whistleCountTop;
+    ImageView topBurnerVessel;
+
 
 
     @Override
@@ -39,6 +44,11 @@ public class DummyFragment extends Fragment {
         crollerTop = (Croller)view.findViewById(R.id.crollers);
         crollerLeft=(Croller)view.findViewById(R.id.crollersLeft);
         crollerRight=(Croller)view.findViewById(R.id.crollersRight);
+
+        whistleCountTop=(ImageView)view.findViewById(R.id.whistle_img);
+        topBurnerVessel=(ImageView)view.findViewById(R.id.topBurnerVessel);
+
+        whistleCountTop.setEnabled(false);
 
         //crollerTop.setAntiClockwise(true);
         //crollerTop.setIndicatorWidth(10);
@@ -62,15 +72,24 @@ public class DummyFragment extends Fragment {
 
                 textView.setText(String.valueOf(progress));
 
+                int i=0;
+
+                if(i==1){
+
+                    topBurnerVessel.setImageDrawable(getResources().getDrawable(R.drawable.vessel));
+
+                }
+
                 if(progress>=0 && progress<=75 ){
+
                     croller.setIndicatorColor(Color.parseColor("#76c9f5"));
                     croller.setProgressPrimaryColor(Color.parseColor("#76c9f5"));
                 }else if(progress>=75 && progress<=130){
                     croller.setIndicatorColor(Color.parseColor("#f3e701"));
                     croller.setProgressPrimaryColor(Color.parseColor("#f3e701"));
                 }else if(progress>=131 && progress<=180){
-                    croller.setIndicatorColor(Color.parseColor("#de7300"));
-                    croller.setProgressPrimaryColor(Color.parseColor("#de7300"));
+                    croller.setIndicatorColor(Color.parseColor("#d32f2f"));
+                    croller.setProgressPrimaryColor(Color.parseColor("#d32f2f"));
                 }
 
             }
