3983fbcd81afa7a10fcaefc1de4d89b07441f62d
murali <saravanamurali24@gmail.com>
2019-12-07 15:46:57 +0530

    Call to mobile number changes commit

--- app/src/main/java/com/clj/blesample/operation/MenuActivity.java
+++ app/src/main/java/com/clj/blesample/operation/MenuActivity.java
@@ -1,8 +1,16 @@
 package com.clj.blesample.operation;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -10,7 +18,9 @@ import com.clj.blesample.R;
 
 public class MenuActivity extends AppCompatActivity {
 
-    RelativeLayout userPatternBlock,gasConsumptionBlock,balanceGasBlock,historyOfMaintenanceBlock,gasRefillBlock,customerServiceBlock;
+    RelativeLayout userPatternBlock, gasConsumptionBlock, balanceGasBlock, historyOfMaintenanceBlock, gasRefillBlock, customerServiceBlock;
+
+    public static final int REQUEST_PHONE_CALL=121;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +28,70 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
 
+        initView();
 
 
-        Toast.makeText(MenuActivity.this,"Menu Activity Called",Toast.LENGTH_LONG).show();
+        customerServiceBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+
+                builder.setTitle("Customer Service Assistance");
+
+                builder.setMessage("Do you want to?");
+
+                builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String phone = "9123521374";
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                        if (ContextCompat.checkSelfPermission(MenuActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(MenuActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+                        }
+                        else
+                        {
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+
+                builder.setNegativeButton("Message", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.cancel();
+                    }
+                });
+
+
+                builder.setNeutralButton("Cancel!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog diag = builder.create();
+                diag.show();
+
+
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private void initView() {
+
+        customerServiceBlock=(RelativeLayout)findViewById(R.id.callCenterServiceBlock);
 
     }
 }
