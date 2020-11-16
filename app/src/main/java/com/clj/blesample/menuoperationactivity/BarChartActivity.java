87e010cf0ad9bf7fc5f74c973b2bcd64141eca96
murali <murali.e@sosaley.in>
2020-11-16 15:00:51 +0530

    Difference between two dates commit

--- app/src/main/java/com/clj/blesample/menuoperationactivity/BarChartActivity.java
+++ app/src/main/java/com/clj/blesample/menuoperationactivity/BarChartActivity.java
@@ -1,6 +1,7 @@
 package com.clj.blesample.menuoperationactivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -10,25 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.clj.blesample.R;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 
 public class BarChartActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -42,10 +38,12 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
 
     DatePickerDialog.OnDateSetListener setListenerFromDate, setListenerToDate;
     TextView fromDate, toDate;
+    Button submit;
 
-    
+
     LineGraphSeries<DataPoint> series;
     SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yyyy");
+    private String selectedFromDate, selectedToDate, selectedBurner;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -58,6 +56,25 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
 
         fromDate = (TextView) findViewById(R.id.fromDate);
         toDate = (TextView) findViewById(R.id.toDate);
+        submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (selectedFromDate != null && selectedToDate != null && selectedBurner != null) {
+                    Intent intent = new Intent(BarChartActivity.this, GasConsumptionActivity.class);
+                    intent.putExtra("FROMDATE", selectedFromDate);
+                    intent.putExtra("TODATE", selectedToDate);
+                    intent.putExtra("BURNER", selectedBurner);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(BarChartActivity.this, "Please Select", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
 
 
         spinner.setOnItemSelectedListener(this);
@@ -65,9 +82,9 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(ad);
 
-        graphView = (GraphView) findViewById(R.id.graphView);
+        /*graphView = (GraphView) findViewById(R.id.graphView);
         series = new LineGraphSeries<>(getDataPoint());
-        graphView.addSeries(series);
+        graphView.addSeries(series);*/
 
 
         Calendar calendar = Calendar.getInstance();
@@ -104,6 +121,7 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
                 month = month + 1;
                 String date = dayOfMonth + "/" + month + "/" + year;
                 fromDate.setText(date);
+                selectedFromDate = date;
 
             }
         };
@@ -116,11 +134,33 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
                 month = month + 1;
                 String date = dayOfMonth + "/" + month + "/" + year;
                 toDate.setText(date);
+                selectedToDate = date;
 
             }
         };
 
-        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String burner = parent.getItemAtPosition(position).toString();
+        System.out.println("SelectedBurners" + burner);
+        selectedBurner = burner;
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+}
+
+
+/*graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
                                                                @Override
                                                                public String formatLabel(double value, boolean isValueX) {
 
@@ -140,45 +180,44 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
         ArrayList<String> xAXES = new ArrayList<>();
         ArrayList<Entry> yAXESsin = new ArrayList<>();
         ArrayList<Entry> yAXEScos = new ArrayList<>();
-        double x = 0 ;
+        double x = 0;
         int numDataPoints = 50;
 
-        for(int i=0;i<numDataPoints;i++){
+        for (int i = 0; i < numDataPoints; i++) {
             float sinFunction = Float.parseFloat(String.valueOf(Math.sin(x)));
             float cosFunction = Float.parseFloat(String.valueOf(Math.cos(x)));
             x = x + 1;
-            yAXESsin.add(new Entry(sinFunction,i));
-            yAXEScos.add(new Entry(cosFunction,i));
+            yAXESsin.add(new Entry(sinFunction, i));
+            yAXEScos.add(new Entry(cosFunction, i));
             xAXES.add(i, String.valueOf(x));
         }
 
         String[] xaxes = new String[xAXES.size()];
-        for(int i=0; i<xAXES.size();i++){
+        for (int i = 0; i < xAXES.size(); i++) {
             xaxes[i] = xAXES.get(i).toString();
         }
 
         ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
 
-        LineDataSet lineDataSet1 = new LineDataSet(yAXEScos,"cos");
+        LineDataSet lineDataSet1 = new LineDataSet(yAXEScos, "cos");
         lineDataSet1.setDrawCircles(false);
         lineDataSet1.setColor(Color.BLUE);
 
-        LineDataSet lineDataSet2 = new LineDataSet(yAXESsin,"sin");
+        LineDataSet lineDataSet2 = new LineDataSet(yAXESsin, "sin");
         lineDataSet2.setDrawCircles(false);
         lineDataSet2.setColor(Color.RED);
 
         lineDataSets.add(lineDataSet1);
         lineDataSets.add(lineDataSet2);
 
-       // lineChart.setData(new LineData(xaxes,lineDataSets));
+        // lineChart.setData(new LineData(xaxes,lineDataSets));
 
         lineChart.setData(new LineData(lineDataSets));
 
         lineChart.setVisibleXRangeMaximum(65f);
 
-    }
 
-    private DataPoint[] getDataPoint() {
+         private DataPoint[] getDataPoint() {
 
         System.out.println("DateFormet " + new Date());
 
@@ -202,18 +241,4 @@ public class BarChartActivity extends AppCompatActivity implements AdapterView.O
         return dp;
 
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String burner = parent.getItemAtPosition(position).toString();
-        System.out.println("SelectedBurners" + burner);
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-
-}
+*/
--- app/src/main/java/com/clj/blesample/menuoperationactivity/GasConsumptionActivity.java
+++ app/src/main/java/com/clj/blesample/menuoperationactivity/GasConsumptionActivity.java
@@ -1,5 +1,6 @@
 package com.clj.blesample.menuoperationactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,8 +10,12 @@ import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class GasConsumptionActivity extends AppCompatActivity {
 
@@ -21,16 +26,30 @@ public class GasConsumptionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas_consumption);
 
-        int y,x;
-        x=0;
-        y=0;
+        Intent intent = getIntent();
+        String fromDate = intent.getStringExtra("FROMDATE");
+        String toDate = intent.getStringExtra("TODATE");
+        intent.getStringExtra("BURNER");
+        System.out.println("stringDate" + fromDate);
+
+        GraphView graph = (GraphView) findViewById(R.id.graphView);
+        series = new LineGraphSeries<DataPoint>();
+
+
+        long numberOfDays = findDifferenceBetweenDates(fromDate, toDate);
+        int noOfDays = (int) numberOfDays;
+
+
+
+
 
         // generate Dates
         Calendar calendar = Calendar.getInstance();
         Date d1 = calendar.getTime();
-      // int d1= calendar.get(Calendar.DAY_OF_MONTH);
+        // int d1= calendar.get(Calendar.DAY_OF_MONTH);
 
-        System.out.println("DayAndMonth"+d1);
+
+        System.out.println("DayAndMonth" + d1);
 
         calendar.add(Calendar.DATE, 1);
         Date d2 = calendar.getTime();
@@ -38,59 +57,27 @@ public class GasConsumptionActivity extends AppCompatActivity {
         calendar.add(Calendar.DATE, 1);
         Date d3 = calendar.getTime();
 
-        calendar.add(Calendar.DATE, 1);
-        Date d4 = calendar.getTime();
 
-        calendar.add(Calendar.DATE, 1);
-        Date d5 = calendar.getTime();
-
-        calendar.add(Calendar.DATE, 1);
-        Date d6 = calendar.getTime();
-
-        calendar.add(Calendar.DATE, 1);
-        Date d7 = calendar.getTime();
-
-        calendar.add(Calendar.DATE, 1);
-        Date d8 = calendar.getTime();
-
-        calendar.add(Calendar.DATE, 1);
-        Date d9 = calendar.getTime();
-
-        calendar.add(Calendar.DATE, 1);
-        Date d10 = calendar.getTime();
-
-        GraphView graph=(GraphView) findViewById(R.id.graphView);
-        series=new LineGraphSeries<DataPoint>();
-
-        System.out.println("AllDate"+d1+" "+d2+" "+d3+" "+d4+" "+d5+" "+d6+" "+d7+" "+d8+" "+d9+" "+d10);
+        // System.out.println("AllDate" + d1 + " " + d2 + " " + d3 + " " + d4 + " " + d5 + " " + d6 + " " + d7 + " " + d8 + " " + d9 + " " + d10);
 
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
                 new DataPoint(d1, 10),
                 new DataPoint(d2, 2),
                 new DataPoint(d3, 8),
-                new DataPoint(d4, 1),
-                new DataPoint(d5, 9),
-                new DataPoint(d6, 3),
-                new DataPoint(d7, 10),
-                new DataPoint(d8, 2),
-                new DataPoint(d9, 7),
-                new DataPoint(d10, 5)
 
         });
-
-
-
 
         graph.addSeries(series);
 
         // set date label formatter
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(GasConsumptionActivity.this));
-        graph.getGridLabelRenderer().setNumHorizontalLabels(10); // only 4 because of the space
+        graph.getGridLabelRenderer().setNumHorizontalLabels(noOfDays); // only 4 because of the space
 
 // set manual x bounds to have nice steps
         graph.getViewport().setMinX(d1.getTime());
-        graph.getViewport().setMaxX(d10.getTime());
+        graph.getViewport().setMaxX(d2.getTime());
+
         graph.getViewport().setXAxisBoundsManual(false);
 
 
@@ -110,6 +97,34 @@ public class GasConsumptionActivity extends AppCompatActivity {
         graph.getViewport().setMinY(0);
         graph.getViewport().setMaxX(15);*/
 
+
+    }
+
+    private long findDifferenceBetweenDates(String fromDate, String toDate) {
+
+        long difference_In_Days = 0;
+
+        try {
+
+            Date fromDateFormet = new SimpleDateFormat("dd/MM/yyyy").parse(fromDate);
+            Date toDateFormet = new SimpleDateFormat("dd/MM/yyyy").parse(toDate);
+
+
+            System.out.println("StringToDateConversion" + fromDateFormet); // Sat Jan 02 00:00:00 GMT 2010
+            System.out.println("StringToDateConversion" + toDateFormet);
+
+            long difference_In_Time = toDateFormet.getTime() - fromDateFormet.getTime();
+
+            difference_In_Days = (difference_In_Time / (1000 * 60 * 60 * 24)) % 365;
+
+            System.out.println("NumberOfDays" + difference_In_Days);
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return difference_In_Days;
 
     }
 }
