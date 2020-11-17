81fe65a64b92562b3ae8a45f3b3047ec272c167a
murali <murali.e@sosaley.in>
2020-11-17 15:19:52 +0530

    New Home Page Commit

--- app/src/main/java/com/clj/blesample/DummyFragment1.java
+++ app/src/main/java/com/clj/blesample/DummyFragment1.java
@@ -10,12 +10,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.clj.blesample.menuoperationactivity.MenuActivity;
 
 
 public class DummyFragment1 extends Fragment {
 
+
+    ImageView leftBurner,leftBurnerVessel,leftBurnerTimer,leftBurnerWhistle,leftBurnerVesselEmpty;
+    Button leftBurnerSetting,leftBurnerEdit;
+
+    ImageView centerBurner,centerBurnerVessel,centerBurnerTimer,centerBurnerWhistle,centerBurnerVesselEmpty;
+    Button centerBurnerSetting,centerBurnerEdit;
+
+    ImageView rightBurner,rightBurnerVessel,rightBurnerTimer,rightBurnerWhistle,rightBurnerVesselEmpty;
+    Button rightBurnerSetting,rightBurnerEdit;
+
+    LinearLayout leftBurnerWidgetBlock,centerBurnerWidgetBlock,rightBurnerWidgetBlock;
 
 
 
@@ -25,9 +37,49 @@ public class DummyFragment1 extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dummy, container, false);
 
+        initView(view);
 
 
         return view;
+    }
+
+    private void initView(View view) {
+
+        leftBurner=(ImageView)view.findViewById(R.id.leftBurner);
+        leftBurnerVessel=(ImageView)view.findViewById(R.id.leftBurnerVessel);
+        leftBurnerTimer=(ImageView)view.findViewById(R.id.leftBurnerTimer);
+        leftBurnerWhistle=(ImageView)view.findViewById(R.id.leftBurnerWhistle);
+        leftBurnerVesselEmpty=(ImageView)view.findViewById(R.id.leftBurnerVesselEmpty);
+
+        leftBurnerSetting=(Button)view.findViewById(R.id.leftBurnerSetting);
+        leftBurnerEdit=(Button)view.findViewById(R.id.leftBurnerEdit);
+
+
+        centerBurner=(ImageView)view.findViewById(R.id.centerBurner);
+        centerBurnerVessel=(ImageView)view.findViewById(R.id.centerBurnerVessel);
+        centerBurnerTimer=(ImageView)view.findViewById(R.id.centerBurnerTimer);
+        centerBurnerWhistle=(ImageView)view.findViewById(R.id.centerBurnerWhistle);
+        centerBurnerVesselEmpty=(ImageView)view.findViewById(R.id.centerBurnerVesselEmpty);
+
+        centerBurnerSetting=(Button)view.findViewById(R.id.centerBurnerSetting);
+        centerBurnerEdit=(Button)view.findViewById(R.id.centerBurnerEdit);
+
+        rightBurner=(ImageView)view.findViewById(R.id.rightBurner);
+        rightBurnerVessel=(ImageView)view.findViewById(R.id.rightBurnerVessel);
+        rightBurnerTimer=(ImageView)view.findViewById(R.id.rightBurnerTimer);
+        rightBurnerWhistle=(ImageView)view.findViewById(R.id.rightBurnerWhistle);
+        rightBurnerVesselEmpty=(ImageView)view.findViewById(R.id.rightBurnerVesselEmpty);
+
+        rightBurnerSetting=(Button)view.findViewById(R.id.rightBurnerSetting);
+        rightBurnerEdit=(Button)view.findViewById(R.id.rightBurnerEdit);
+
+        leftBurnerWidgetBlock=(LinearLayout)view.findViewById(R.id.leftBurnerWidgetsBlock);
+        centerBurnerWidgetBlock=(LinearLayout)view.findViewById(R.id.centerBurnerWidgetsBlock);
+        rightBurnerWidgetBlock=(LinearLayout)view.findViewById(R.id.rightBurnerWidgetsBlock);
+
+
+
+
     }
 
     private void launchMenuActivity() {
