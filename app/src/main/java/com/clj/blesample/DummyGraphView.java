c7f7e10e54da1104b5cb6316e7fbf3fe39cf1bf1
murali <murali.e@sosaley.in>
2020-11-18 13:38:55 +0530

    Operation Edit Screen Commit

---
+++ app/src/main/java/com/clj/blesample/DummyGraphView.java
@@ -0,0 +1,80 @@
+package com.clj.blesample;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+
+public class DummyGraphView extends AppCompatActivity {
+
+    LineChartView lineChartView;
+
+    String[]  axisData = {"10/11/2020", "11/11/2020", "12/11/2020", "13/11/2020", "14/11/2020", "15/11/2020", "16/11/2020" };
+
+
+    int[] yAxisData = {80, 30, 70, 20, 15, 50, 10};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_graph_view);
+
+        lineChartView = findViewById(R.id.chart);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+        Line line = new Line(yAxisValues);
+
+        for(int i = 0; i < axisData.length; i++){
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++){
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        lineChartView.setLineChartData(data);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setHasSeparationLine(true);
+
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        data.setAxisYLeft(yAxis);
+
+        line.setColor(Color.parseColor("#9C27B0"));
+
+        axis.setTextSize(16);
+
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+
+        yAxis.setName("GasUsage in KG");
+
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top = 110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+    }
+}
